508866ee7bdb7cb8e39ddc6a00a147c6e5287f4b
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-30 00:58:28 -0400

    display error message if submit attempted without input

--- todo-app/src/components/TodoForm.jsx
+++ todo-app/src/components/TodoForm.jsx
@@ -7,50 +7,59 @@ import moment from "moment"
 export default function TodoForm() {
   const dispatch = useDispatch()
   const inputText = useSelector(state => state.inputText)
+  const error = useSelector(state => state.error)
+
   return (
-    <div className="TodoForm">
-      <div>
-        <form
-          className="form"
-          onSubmit={e => {
-            e.preventDefault()
-            dispatch({
-              type: "SUBMIT_TODO",
-              payload: {
-                id: Date.now(),
-                task: inputText,
-                completed: false,
-                completeDate: moment().format("MMM Do YYYY, h:mm:ss a")
+    <div className="container">
+      <div className="TodoForm">
+        <div>
+          <form
+            className="form"
+            onSubmit={e => {
+              e.preventDefault()
+              if (!inputText) {
+                dispatch({ type: "SUBMIT_FAILED" })
               }
-            })
-          }}
-        >
-          <input
-            className="form-input"
-            type="text"
-            placeholder="add todo..."
-            onChange={e => {
               dispatch({
-                type: "CHANGE_INPUT",
-                payload: e.target.value
+                type: "SUBMIT_TODO",
+                payload: {
+                  id: Date.now(),
+                  task: inputText,
+                  completed: false,
+                  completeDate: moment().format("MMM Do YYYY, h:mm:ss a")
+                }
               })
             }}
-            value={inputText}
-          ></input>
-          <button className="add-todo-btn" type="submit">
-            <FontAwesomeIcon icon={faPlus} />
-          </button>
-        </form>
-      </div>
+          >
+            <input
+              className="form-input"
+              type="text"
+              placeholder="add todo..."
+              onChange={e => {
+                dispatch({
+                  type: "CHANGE_INPUT",
+                  payload: e.target.value
+                })
+              }}
+              value={inputText}
+            ></input>
 
-      <button
-        className="clear-completed-btn"
-        onClick={() => {
-          dispatch({ type: "CLEAR_COMPLETED" })
-        }}
-      >
-        <FontAwesomeIcon icon={faTrash} />
-      </button>
+            <button className="add-todo-btn" type="submit">
+              <FontAwesomeIcon icon={faPlus} />
+            </button>
+          </form>
+        </div>
+
+        <button
+          className="clear-completed-btn"
+          onClick={() => {
+            dispatch({ type: "CLEAR_COMPLETED" })
+          }}
+        >
+          <FontAwesomeIcon icon={faTrash} />
+        </button>
+      </div>
+      {error && <p className="error">Please enter a task</p>}
     </div>
   )
 }
